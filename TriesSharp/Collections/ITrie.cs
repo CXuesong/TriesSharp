5460be8688ae8d636b84ac275fd1c14e1157f991
forest93 <forest93@163.com>
2022-01-19 00:23:43 +0800

    Set up basic API.

---
+++ TriesSharp/Collections/ITrie.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriesSharp.Collections
+{
+    /// <summary>
+    /// Represents a collection of string key / value pairs that can be retrieved by prefix. 
+    /// </summary>
+    /// <typeparam name="TValue">type of the values.</typeparam>
+    public interface ITrie<TValue> : IReadOnlyTrie<TValue>, IDictionary<ReadOnlyMemory<char>, TValue>
+    {
+        void Add(ReadOnlySpan<char> key, TValue value);
+
+        void IDictionary<ReadOnlyMemory<char>, TValue>.Add(ReadOnlyMemory<char> key, TValue value) => Add(key.Span, value);
+
+        void Add(string key, TValue value) => Add(key.AsSpan(), value);
+
+        TValue GetOrAdd(ReadOnlyMemory<char> key, TValue value)
+        {
+            if (TryGetValue(key.Span, out var v)) return v;
+            Add(key, value);
+            return value;
+        }
+
+        TValue GetOrAdd(string key, TValue value) => GetOrAdd(key.AsMemory(), value);
+
+        new TValue this[ReadOnlySpan<char> key] { get; set; }
+
+        new TValue this[string key]
+        {
+            get => this[key.AsSpan()];
+            set => this[key.AsSpan()] = value;
+        }
+
+        bool IDictionary<ReadOnlyMemory<char>, TValue>.TryGetValue(ReadOnlyMemory<char> key, [MaybeNullWhen(false)] out TValue value)
+            => TryGetValue(key.Span, out value);
+
+        TValue IDictionary<ReadOnlyMemory<char>, TValue>.this[ReadOnlyMemory<char> key]
+        {
+            get => this[key.Span];
+            set => this[key.Span] = value;
+        }
+    }
+
+    public interface IReadOnlyTrie<TValue> : IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>
+    {
+        TValue this[ReadOnlySpan<char> key] { get; }
+
+        TValue IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>.this[ReadOnlyMemory<char> key] => this[key.Span];
+
+        TValue this[string key] => this[key.AsSpan()];
+
+        bool TryGetValue(ReadOnlySpan<char> key, [MaybeNullWhen(false)] out TValue value);
+
+        bool IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>.TryGetValue(ReadOnlyMemory<char> key, [MaybeNullWhen(false)] out TValue value)
+            => TryGetValue(key.Span, out value);
+
+        bool TryGetValue(string key, [MaybeNullWhen(false)] out TValue value)
+            => TryGetValue(key.AsSpan(), out value);
+
+        /// <summary>
+        /// Retrieves the value in the longest prefix of the specified query that exists in the trie.
+        /// </summary>
+        /// <param name="query">a character sequence.</param>
+        /// <returns>corresponding value under the key <c>query[..prefixLength]</c>.</returns>
+        (int PrefixLength, TValue value) MatchLongestPrefix(ReadOnlySpan<char> query);
+
+        /// <inheritdoc cref="MatchLongestPrefix(ReadOnlySpan{char})"/>
+        (int PrefixLength, TValue value) MatchLongestPrefix(ReadOnlyMemory<char> query) => MatchLongestPrefix(query.Span);
+
+        /// <inheritdoc cref="MatchLongestPrefix(ReadOnlySpan{char})"/>
+        /// <exception cref="NullReferenceException"><paramref name="query"/> is <c>null</c>.</exception>
+        (int PrefixLength, TValue value) MatchLongestPrefix(string query) => MatchLongestPrefix(query.AsSpan());
+
+        IEnumerable<KeyValuePair<ReadOnlyMemory<char>, TValue>> EnumEntriesFromPrefix(ReadOnlySpan<char> prefix);
+
+        IEnumerable<KeyValuePair<ReadOnlyMemory<char>, TValue>> EnumEntriesFromPrefix(ReadOnlyMemory<char> prefix) => EnumEntriesFromPrefix(prefix.Span);
+
+        /// <exception cref="NullReferenceException"><paramref name="prefix"/> is <c>null</c>.</exception>
+        IEnumerable<KeyValuePair<ReadOnlyMemory<char>, TValue>> EnumEntriesFromPrefix(string prefix) => EnumEntriesFromPrefix(prefix.AsSpan());
+    }
+}
---
+++ TriesSharp/Collections/Trie.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriesSharp.Collections
+{
+    public abstract class Trie<TValue> : ITrie<TValue>
+    {
+
+        /// <inheritdoc />
+        public abstract IEnumerator<KeyValuePair<ReadOnlyMemory<char>, TValue>> GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <inheritdoc />
+        public abstract void Add(KeyValuePair<ReadOnlyMemory<char>, TValue> item);
+
+        /// <inheritdoc />
+        public abstract void Clear();
+
+        /// <inheritdoc />
+        bool ICollection<KeyValuePair<ReadOnlyMemory<char>, TValue>>.Contains(KeyValuePair<ReadOnlyMemory<char>, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public abstract void CopyTo(KeyValuePair<ReadOnlyMemory<char>, TValue>[] array, int arrayIndex);
+
+        /// <inheritdoc />
+        public abstract bool Remove(KeyValuePair<ReadOnlyMemory<char>, TValue> item);
+
+        /// <inheritdoc cref="ICollection{T}.Count" />
+        public abstract int Count { get; }
+
+        /// <inheritdoc />
+        bool ICollection<KeyValuePair<ReadOnlyMemory<char>, TValue>>.IsReadOnly => false;
+
+        /// <inheritdoc />
+        bool IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>.ContainsKey(ReadOnlyMemory<char> key)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public abstract bool Remove(ReadOnlyMemory<char> key);
+
+        /// <inheritdoc />
+        bool IDictionary<ReadOnlyMemory<char>, TValue>.ContainsKey(ReadOnlyMemory<char> key)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        IEnumerable<ReadOnlyMemory<char>> IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>.Keys => Keys;
+
+        /// <inheritdoc />
+        public ICollection<TValue> Values { get; } = new ValueCollection();
+
+        /// <inheritdoc />
+        public ICollection<ReadOnlyMemory<char>> Keys { get; } = new KeyCollection();
+
+        /// <inheritdoc />
+        IEnumerable<TValue> IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>.Values => Values;
+
+        /// <inheritdoc cref="ITrie{TValue}.this[ReadOnlySpan{char}]" />
+        public abstract TValue this[ReadOnlySpan<char> key] { get; set; }
+
+        /// <inheritdoc />
+        public abstract bool TryGetValue(ReadOnlySpan<char> key, out TValue value);
+
+        /// <inheritdoc />
+        public abstract (int PrefixLength, TValue value) MatchLongestPrefix(ReadOnlySpan<char> query);
+
+        /// <inheritdoc />
+        public abstract IEnumerable<KeyValuePair<ReadOnlyMemory<char>, TValue>> EnumEntriesFromPrefix(ReadOnlySpan<char> prefix);
+
+        /// <inheritdoc />
+        public abstract void Add(ReadOnlySpan<char> key, TValue value);
+
+        private sealed class KeyCollection : ICollection<ReadOnlyMemory<char>>
+        {
+            /// <inheritdoc />
+            public IEnumerator<ReadOnlyMemory<char>> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            /// <inheritdoc />
+            public void Add(ReadOnlyMemory<char> item)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void Clear()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public bool Contains(ReadOnlyMemory<char> item)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void CopyTo(ReadOnlyMemory<char>[] array, int arrayIndex)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public bool Remove(ReadOnlyMemory<char> item)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public int Count { get; }
+
+            /// <inheritdoc />
+            public bool IsReadOnly { get; }
+        }
+
+        private sealed class ValueCollection : ICollection<TValue>
+        {
+            /// <inheritdoc />
+            public IEnumerator<TValue> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            /// <inheritdoc />
+            public void Add(TValue item)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void Clear()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public bool Contains(TValue item)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void CopyTo(TValue[] array, int arrayIndex)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public bool Remove(TValue item)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public int Count { get; }
+
+            /// <inheritdoc />
+            public bool IsReadOnly { get; }
+        }
+    }
+}

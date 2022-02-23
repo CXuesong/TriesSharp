c429019b6e6b63b3bc389675bd59d6fc78824e9e
forest93 <forest93@163.com>
2022-02-24 00:16:33 +0800

    Add benchmark project.

---
+++ TriesSharp.Benchmarking/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using TriesSharp.Tests.UnitTestProject1.Benchmarks;
+
+var config = DefaultConfig.Instance
+    .WithSummaryStyle(SummaryStyle.Default.WithMaxParameterColumnWidth(100))
+    .AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig()));
+BenchmarkSwitcher.FromAssembly(typeof(TrieBenchmarks).Assembly).Run(args, config);
---
+++ TriesSharp.Tests.UnitTestProject1/Benchmarks/TrieBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using TriesSharp.Collections;
+
+namespace TriesSharp.Tests.UnitTestProject1.Benchmarks;
+
+public class TrieBenchmarks
+{
+
+    [Benchmark]
+    [ArgumentsSource(nameof(GetLoadTrieTestArguments))]
+    public void LoadTrie(List<string> wordList)
+    {
+        var trie = new Trie<int>();
+        for (int i = 0; i < wordList.Count; i++)
+            trie[wordList[i]] = i;
+    }
+
+    public static IEnumerable<List<string>> GetLoadTrieTestArguments()
+    {
+        yield return TextResourceLoader.LoadWordList(TextResourceLoader.ShijiSnippet1);
+        yield return TextResourceLoader.LoadWordList(TextResourceLoader.TaleOfTwoCities1);
+        yield return TextResourceLoader.LoadWordList(TextResourceLoader.WiktionaryTopFreq1000);
+    }
+
+}
